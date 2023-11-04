45f6501270546b7f374a8aeab025e9caa76458a2
Renan de Carvalho Rinaldi <renan.crinaldi@gmail.com>
2023-11-04 20:39:34 -0300

    feat: implementando output in networks

--- Networks/main.tf
+++ Networks/main.tf
@@ -17,7 +17,7 @@ resource "aws_subnet" "public_subnets" {
  }
 }
 
-resource "aws_subnet" "private_subnet" {
+resource "aws_subnet" "private_subnets" {
  count = "${length(var.private_subnet_cidrs)}"  
  vpc_id = aws_vpc.main.id
  cidr_block = "${element(var.private_subnet_cidrs, count.index)}"
--- Networks/output.tf
+++ Networks/output.tf
@@ -1,4 +1,19 @@
-output "vpcs" {
+output "vpc_id" {
   description = "VPC Outputs"
-  value = { for vpc in aws_vpc.main : vpc.tags.Name => { "cidr_block" : vpc.cidr_block, "id" : vpc.id } }
+  value = "${aws_vpc.main.id}"
+}
+
+output "public_subnet_id" {
+  value = aws_subnet.public_subnets[*].id
+  description = "The ID of the subnet Outputs"
+}
+
+output "private_subnet_id" {
+  value = aws_subnet.private_subnets[*].id
+  description = "The ID of the private subnet Outputs"
+}
+
+output "security_group_id" {
+  description = "The ID of the security group Outputs"
+  value = aws_security_group.tech_challenge_sg.id
 }
