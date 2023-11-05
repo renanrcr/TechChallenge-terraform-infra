5b33e93d801bb43e2078918769f0a4a5cedb39f6
Renan de Carvalho Rinaldi <renan.crinaldi@gmail.com>
2023-11-05 08:44:02 -0300

    feat: add dynamo db

--- SqlServer/main.tf
+++ SqlServer/main.tf
@@ -14,3 +14,26 @@ resource "aws_db_instance" "sqlserver_default" {
    Name = "${var.environment}-sqlserver"
  }
 }
+
+resource "aws_dynamodb_table" "customer_db" {
+  name = "customers_cache"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "userId"
+  stream_enabled = false
+  table_class = "STANDARD"
+  deletion_protection_enabled = false
+  ttl {
+    enabled = true
+    attribute_name = "ttl"
+  }
+  attribute {
+    name = "userId"
+    type = "S"
+  }
+  server_side_encryption {
+    enabled = true
+  }
+  tags = {
+    Name = "DynamoDB"
+  }
+}
